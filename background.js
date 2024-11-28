0b0fee68bd3720a3ab044dc73143fe1266608deb
phatIntern <phat.pham@saleshood.com>
2024-11-28 15:13:13 +0700

    quick share as sidedish of blogging

--- background.js
+++ background.js
@@ -1,27 +1,33 @@
+chrome.action.onClicked.addListener(() => {
+    chrome.runtime.openOptionsPage();
+});
+
 
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-
+    let tabId = null;
 
     // Handle the message
     console.log("Message received:", message);
+    async function executeInAutomatedTab(callback, interval = 1000) {
+        return new Promise(resolve => setTimeout(async () => {
+            await chrome.scripting.executeScript({
+                target: { tabId },
+                world: 'MAIN',
+                function: callback,
+            });
+            resolve();
+
+        }, interval));
+    }
 
     // Execute the script
     if (message === 'start_automation') {
         // open a new tab
         const tab = await chrome.tabs.create({ url: 'https://dichvucong.gov.vn' });
-        const tabId = tab.id;
+        tabId = tab.id;
 
-        function executeInAutomatedTab(callback, interval = 1000) {
-            return new Promise(resolve => setTimeout(async () => {
-                chrome.scripting.executeScript({
-                    target: { tabId },
-                    world: 'MAIN',
-                    function: callback,
-                });
-
-            }, interval));
-        }
+      
         await new Promise(resolve => setTimeout(() => {
             chrome.scripting.executeScript({
                 target: { tabId },
@@ -57,24 +63,46 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
             });
             resolve();
         }, 1000));
-        executeInAutomatedTab(() => {
+        await executeInAutomatedTab(async () => {
+            async function simulateTyping(inputElement, text, delay = 100) {
+                inputElement.focus();
+                const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+                nativeInputValueSetter.call(inputElement, text);
+                const inputEvent = new Event('input', { bubbles: true });
+                inputElement.dispatchEvent(inputEvent);
+                await new Promise(resolve => setTimeout(resolve, delay));
+            }
+
+            //* edit here
+            const CCCD = undefined;
+            const password = undefined;
+
+            
+            const resolveButton = document.querySelector('button.custom-btn-resolve');
+            if (resolveButton) {
+                resolveButton.click();
+                console.log('Clicked on the resolve button');
+            } else {
+                console.error('The resolve button is not available');
+            }
+            await new Promise(resolve => setTimeout(resolve, 1000));
             const usernameInput = document.querySelector('input#username');
+            console.log("🚀 ~ awaitexecuteInAutomatedTab ~ usernameInput:", usernameInput)
             if (usernameInput) {
-                usernameInput.value = '052097013984';
-                console.log('Typed in the username');
+                simulateTyping(usernameInput, CCCD);
             } else {
                 console.error('The username input is not available');
             }
             // type in input.password
             const passwordInput = document.querySelector('input#password');
             if (passwordInput) {
-                passwordInput.value = 'Trocap123!';
-                console.log('Typed in the password');
+                simulateTyping(passwordInput, password);
             } else {
                 console.error('The password input is not available');
             }
-            const resolveButton = document.querySelector('button.custom-btn-resolve');
+
             if (resolveButton) {
+                await new Promise(resolve => setTimeout(resolve, 1000));
                 resolveButton.click();
                 console.log('Clicked on the resolve button');
             } else {
@@ -84,6 +112,47 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
 
 
 
+    }
+
+    if (message === 'go_to_dktt') {
+        const newTab = await chrome.tabs.create({ url: 'https://dichvucong.bocongan.gov.vn/bo-cong-an/tiep-nhan-online/chon-truong-hop-ho-so?ma-thu-tuc-public=56952' });
+        tabId = newTab.id;
+        executeInAutomatedTab(async () => {
+            console.log('Redirected to the DKTT page');
+            const loaiXeSelect = document.querySelector('select#_fcmaLoaiXe');
+            console.log("🚀 ~ executeInAutomatedTab ~ _fcmaLoaiXe:", loaiXeSelect)
+            if (loaiXeSelect) {
+                loaiXeSelect.selectedIndex = 1;
+                loaiXeSelect.dispatchEvent(new Event('change'));
+                
+            } else {
+                console.error('The vehicle type select is not available');
+            }
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+            const dungTichXeSelect = document.querySelector('select#_fcdungTichXe');
+            if (dungTichXeSelect) {
+                dungTichXeSelect.selectedIndex = 1;
+                dungTichXeSelect.dispatchEvent(new Event('change'));
+            } else {
+                console.error('The vehicle capacity select is not available');
+            }
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            const diaChiDangKySelect = document.querySelector('select#_fcdiaChiDangKy');
+            if (diaChiDangKySelect) {
+                diaChiDangKySelect.selectedIndex = 1;
+                diaChiDangKySelect.dispatchEvent(new Event('change'));
+            }
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            const nextButton = document.querySelector('button#btn-next');
+            if (nextButton) {
+                nextButton.click();
+            } else {
+                console.error('The next button is not available');
+            }
+        }, 1000);
+
+        
     }
 
     // Send a response back
